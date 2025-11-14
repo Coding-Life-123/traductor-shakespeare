8b46b17e244a49f6a7eab40795098c8d76472450
Coding-Life-123 <dfordonezpina@gmail.com>
2025-11-13 20:09:05 -0500

    base styles deleted

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,9 @@ import Traductor from "./components/Traductor.vue";
 
 <template>
 
-  <Traductor msg="Vite + Vue" />
+  <Traductor />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
