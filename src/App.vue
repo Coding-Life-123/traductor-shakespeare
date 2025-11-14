24250afe944347b627e5442580e5c54efe817d35
Coding-Life-123 <dfordonezpina@gmail.com>
2025-11-13 20:00:46 -0500

    component name fixed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <script setup>
-import traductor from "./components/traductor.vue";
+import Traductor from "./components/Traductor.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <traductor msg="Vite + Vue" />
+
+  <Traductor msg="Vite + Vue" />
 </template>
 
 <style scoped>
